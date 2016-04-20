0de5fd2643bc1bd09cb7f1939eae10cb505b4531
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-04-20 13:51:51 -0500

    Update view for form acces

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,53 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+  <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+  <!--[if lte IE 8]>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+  <![endif]-->
+  <!--[if gt IE 8]><!-->
+  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+  <!--<![endif]-->
+  <!--[if lte IE 8]>
+  <link rel="stylesheet" href="css/layouts/blog-old-ie.css">
+  <![endif]-->
+  <!--[if gt IE 8]><!-->
+  <link rel="stylesheet" href="css/blog.css">
+  <!--<![endif]-->
+</head>
+<body>
+  <div class="container">
+    <form class="pure-form pure-form-stacked">
+      <fieldset>
+        <legend>A Stacked Form</legend>
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
+        <label for="email">Email</label>
+        <input id="email" type="email" placeholder="Email">
+
+        <label for="password">Password</label>
+        <input id="password" type="password" placeholder="Password">
+
+        <label for="state">State</label>
+        <select id="state">
+          <option>AL</option>
+          <option>CA</option>
+          <option>IL</option>
+        </select>
+
+        <label for="remember" class="pure-checkbox">
+          <input id="remember" type="checkbox"> Remember me
+        </label>
+
+        <button type="submit" class="pure-button pure-button-primary">Sign in</button>
+      </fieldset>
+    </form>
+  </div>
+</body>
 </html>
