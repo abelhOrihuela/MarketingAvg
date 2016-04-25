2fa84392aaaf13cbc2074b6959ab896f908bbd2c
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-04-25 18:32:42 -0500

    Create Model Profile & relations

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+
+    protected $table='profiles';
+  public function company()
+  {
+      return $this->belongsTo('App\Company', 'comp_id');
+  }
+
+  public function position(){
+    return $this->belongsTo('App\Position', 'pos_id');
+  }
 }
