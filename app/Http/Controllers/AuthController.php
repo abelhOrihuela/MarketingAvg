301f0b183ebcab8ba3bf36c9398874a3d374e3b9
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-04-26 14:21:40 -0500

    Create controller Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+      Session::put('company', $request->company);
+      /*
+        $this->validate($request, [
+            'email'    => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only(['email', 'password']))) {
+            return redirect()->route('auth_show_path')->withErrors('No encontramos al usuario.');
+        }
+        */
+
+        return redirect()->route('company_dashboard_path');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return Response
+     */
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect()->route('auth_show_path');
+    }
+}
