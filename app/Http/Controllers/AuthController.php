d80591ec07c839b5604de18fb04134ab5fc0be50
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-04-27 17:06:05 -0500

    Add show & variable session

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,92 +8,90 @@ use Session;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        return view('welcome');
+  /**
+  * Display a listing of the resource.
+  *
+  * @return Response
+  */
+  public function index()
+  {
+    return view('welcome');
+  }
+
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return Response
+  */
+  public function create()
+  {
+    //
+  }
+
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  Request  $request
+  * @return Response
+  */
+  public function store(Request $request)
+  {
+    $this->validate($request, [
+      'email'    => 'required|email',
+      'password' => 'required',
+    ]);
+
+    if (!auth()->attempt($request->only(['email', 'password']))) {
+      return redirect()->route('company_index_path')->withErrors('No encontramos al usuario.');
     }
+    Session::put('company', $request->company);
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
+    return redirect()->route('company_dashboard_path');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-      Session::put('company', $request->company);
+  /**
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return Response
+  */
+  public function show($id)
+  {
+    //
+  }
 
-        $this->validate($request, [
-            'email'    => 'required|email',
-            'password' => 'required',
-        ]);
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  int  $id
+  * @return Response
+  */
+  public function edit($id)
+  {
+    //
+  }
 
-        if (!auth()->attempt($request->only(['email', 'password']))) {
-            return redirect()->route('company_index_path')->withErrors('No encontramos al usuario.');
-        }
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  Request  $request
+  * @param  int  $id
+  * @return Response
+  */
+  public function update(Request $request, $id)
+  {
+    //
+  }
 
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @return Response
+  */
+  public function destroy()
+  {
+    auth()->logout();
 
-        return redirect()->route('company_dashboard_path');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @return Response
-     */
-    public function destroy()
-    {
-        auth()->logout();
-
-        return redirect()->route('auth_show_path');
-    }
+    return redirect()->route('auth_show_path');
+  }
 }
--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,27 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Company;
 use App\Profile;
 use App\Position;
 use Session;
+use DB;
 
 class CompanyController extends Controller
 {
-    public function index(){
-      $companies=Company::all();
+  public function index(){
+    $companies=Company::all();
 
-      return view('welcome')->with('companies', $companies);
-    }
+    return view('welcome')->with('companies', $companies);
+  }
 
-    public function dashboard(){
-      //return "oalH";
-      //  Session::put('company', $request->company);
+  public function dashboard(){
 
-        $company=Company::where('comp_name', "=", session('company'))->first();
+    $company=Company::where('id', "=", session('company'))->first();
+    $grades=Profile::where("comp_id", "=", session('company'))->groupBy('prof_grade')->get();
 
-      return view('dashboard')->with('company', $company);
-    }
+    return view('dashboard')
+      ->with('company', $company)
+      ->with('grades', $grades);
+  }
+  public function show($id){
+    $companyGrade=DB::table('companies')
+      ->join('profiles', 'companies.id', '=', 'profiles.comp_id')
+      ->where('companies.id', '=', session('company'))
+      ->where('profiles.prof_grade', '=', $id)
+      ->join('positions', 'profiles.pos_id', '=', 'positions.id')
+      ->get();
+
+    return view('show')->with('companyGrade', $companyGrade);
+  }
 }
