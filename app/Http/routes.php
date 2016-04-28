29d833fa92ee5eb8d210e2a1453f752755a1113c
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2016-04-28 15:09:41 -0500

    Add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,14 +22,34 @@ Route::post('/login', [
     'uses' => 'AuthController@store',
     'as'   => 'auth_store_path'
 ]);
+Route::get('/logout', [
+    'uses' => 'AuthController@destroy',
+    'as'   => 'auth_destroy_path'
+]);
 
-Route::get('/dashboard', [
+Route::get('/index', [
     'uses' => 'CompanyController@dashboard',
     'as'   => 'company_dashboard_path'
 ]);
 
 
-Route::get('/{id}', [
+
+Route::get('/grade/{id}', [
     'uses' => 'CompanyController@show',
     'as'   => 'company_show_path'
+]);
+
+Route::get('new/profile',[
+  'uses' => 'ProfileController@store',
+  'as'   => 'profile_new_path'
+]);
+
+Route::post('new/profile',[
+  'uses' => 'ProfileController@create',
+  'as'   => 'profile_create_path'
+]);
+
+Route::get('/dashboard',[
+  'uses' => 'CompanyController@menu',
+  'as'   => 'company_menu_path'
 ]);
