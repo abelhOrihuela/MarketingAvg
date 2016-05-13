9a5fe0c4493c5caf0646e9493317656e4875fa24
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2016-05-13 15:05:40 -0500

    Add view for pdf

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -1,44 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Example 2</title>
-    {!! Html::style('assets/css/pdf.css') !!}
+    <meta charset="utf-8">
+    <title></title>
+      <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+      <link rel="stylesheet" href="css/blog.css">
+      <link rel="stylesheet" href="../css/blog.css">
   </head>
   <body>
 
-    <main>
-      <div id="details" class="clearfix">
-        <div id="invoice">
-          
-          <div class="date">Date of Invoice: {{ $date }}</div>
-        </div>
-      </div>
-      <table border="0" cellspacing="0" cellpadding="0">
-        <thead>
-          <tr>
-            <th class="no">#</th>
-            <th class="desc">DESCRIPTION</th>
-            <th class="unit">UNIT PRICE</th>
-            <th class="total">TOTAL</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="no">{{ $data['quantity'] }}</td>
-            <td class="desc">{{ $data['description'] }}</td>
-            <td class="unit">{{ $data['price'] }}</td>
-            <td class="total">{{ $data['total'] }} </td>
+
+
+    <?php
+
+    $sizeArray=sizeOf($companyGrade);
+
+
+    ?>
+    <div class="img-header">
+        <img src="img/logo.png" alt="" />
+    </div>
+    <h2 class="title-table">Analisis de sueldos</h2>
+
+
+    <table class="pure-table font-table-wtc">
+      <thead>
+        <tr>
+          <th>Grade</th>
+          <th>Position</th>
+          <th>Salary</th>
+          <th>Min</th>
+          <th>Med</th>
+          <th>Max</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        <?php
+        for ($i=0; $i < sizeOf($salaryLowForGrade); $i++) {
+          $salaryMed=$salaryLowForGrade[$i]->prof_salary;
+          for ($j=0; $j < sizeOf($companyGrade); $j++) {
+            if ($salaryLowForGrade[$i]->prof_grade==$companyGrade[$j]->prof_grade) {
+              $salaryMed+=($companyGrade[$j]->prof_progresion * $j);
+              ?>
+
+              <tr>
+                <td class="center">
+                  {{ $companyGrade[$j]->prof_grade}}
+                </td>
+                <td>
+                  {{ $companyGrade[$j]->pos_name }}
+                </td>
+                <td>
+                  $ {{
+                    number_format ( $companyGrade[$j]->prof_salary , 2 , "." , "," )
+                  }}
+                </td>
+                <td>
+                  $ {{ number_format ( ($salaryMed * -$companyGrade[$j]->prof_min) + $salaryMed , 2 , "." , "," )
+                }}
+              </td>
+              <td>
+                $ {{ number_format ( $salaryMed , 2 , "." , "," )  }}
+              </td>
+              <td>
+                $ {{ number_format (($salaryMed * $companyGrade[$j]->prof_min) + $salaryMed , 2 , "." , "," )
+              }}
+            </td>
           </tr>
 
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="2"></td>
-            <td >TOTAL</td>
-            <td>$6,500.00</td>
-          </tr>
-        </tfoot>
-      </table>
+              <?php
+            }
+          }
+        }
+
+
+          ?>
+
+
+
+    </tbody>
+    </table>
+<footer class="footer-table">
+  <br>
+    Toda la información contenida dentro de este documento es confidencial.
+    <br>
+  World Talent Consulting Copyright (c) 2015 Copyright Holder All Rights Reserved.
+</footer>
+
   </body>
 </html>
